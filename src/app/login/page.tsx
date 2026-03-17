67b34de24fc1410b35842ef99c27848b5eb80a23
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-03-17 23:11:51 +0530

    Minor changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,7 +28,7 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter email"
+              placeholder="Enter your email"
               required
             />
           </div>
