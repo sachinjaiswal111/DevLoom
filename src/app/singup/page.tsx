b0b8b04d5f859001f469a91f010d7fd1cf573588
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-03-17 19:37:15 +0530

    minor changes

--- src/app/singup/page.tsx
+++ src/app/singup/page.tsx
@@ -35,7 +35,7 @@ const Signup = () => {
                         <input
                             type="password"
                             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Confirm your password"
+                            placeholder="Confirm password"
                         />
                     </div>
                     <button
