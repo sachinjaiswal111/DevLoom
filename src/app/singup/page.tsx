63bf13cff84904df311da2b039297b9de602afae
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-03-17 19:39:17 +0530

    minor changes

--- src/app/singup/page.tsx
+++ src/app/singup/page.tsx
@@ -25,7 +25,7 @@ const Signup = () => {
                         <input
                             type="password"
                             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Enter your password"
+                            placeholder="Enter password"
                         />
                     </div>
                     <div>
