f791fad3b2970510cb73fdfce5280efdf233f621
sachinjaiswal111 <tryy.sachin@gmail.com>
2026-03-31 17:28:06 +0530

    start Ai configure

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,9 +12,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 // });
 
+export default clerkMiddleware();
 
 
-export const config = {
+
+export  const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
